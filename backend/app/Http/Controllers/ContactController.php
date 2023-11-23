e2bd2c7e7ac342258b8d93f50ab4fa1088894fa7
Husseini <mhmd.husseinyy@gmail.com>
2023-11-23 16:24:15 +0200

    BE<> Create the delete contact api

--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Contact;
 
 class ContactController extends Controller{
     
     public function add(Request $request){
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'first_name' => 'required',
             'last_name' => 'required',
             'email' => 'required|email|unique:contacts',
             'phone_number' => 'required|regex:/^\+[0-9]{1,3}[0-9]{4,14}$/',
-        ]);        
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
         $contact = Contact::create($request->all());
         return response()->json($contact, 201);
     }
@@ -21,10 +26,17 @@ class ContactController extends Controller{
         if ($id === null) {
             $contacts = Contact::all();
             return response()->json($contacts, 200);
+
         } else {
             $contact = Contact::findOrFail($id);
             return response()->json($contact, 200);
         }
+    }
+
+    public function delete($id){
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return response()->json(null, 204);
     }
 
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\ContactController;
 
 Route::post("add", [ContactController::class, "add"]);
 Route::get('contacts/{id?}', [ContactController::class, 'get']);
+Route::delete('/contacts/{id}', [ContactController::class, 'delete']);
