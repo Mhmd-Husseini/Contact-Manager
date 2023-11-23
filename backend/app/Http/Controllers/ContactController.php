f69d81ccaf8b2d1ee3e65a28d20f6d7a5e329556
Husseini <mhmd.husseinyy@gmail.com>
2023-11-23 15:35:19 +0200

    BE<> Create add contact api

--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
-class ContactController extends Controller
-{
-    //
+class ContactController extends Controller{
+    
+    public function add(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:contacts',
+            'phone_number' => 'required|regex:/^\+[0-9]{1,3}[0-9]{4,14}$/',
+        ]);        
+        $contact = Contact::create($request->all());
+        return response()->json($contact, 201);
+    }
 }
