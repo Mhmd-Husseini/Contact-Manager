ae31d4e0f2abb22e077343f9671ab86d164e697c
Husseini <mhmd.husseinyy@gmail.com>
2023-11-24 20:42:11 +0200

    FE<> Add delete contact functionality

--- frontend/app/components/Contact.jsx
+++ frontend/app/components/Contact.jsx
@@ -1,7 +1,20 @@
 import { FaEdit, FaTrash } from 'react-icons/fa';
+import {deleteContact} from './../apis';
 
 const Contact = ({ contact, index }) => {
-  const { first_name, last_name, email, phone_number } = contact;
+  const { id, first_name, last_name, email, phone_number } = contact;
+
+  const handleDelete = async () => {
+    const isConfirmed = window.confirm('Are you sure you want to delete this contact?');
+    if (isConfirmed) {
+      const deleted = await deleteContact(id);
+      if (deleted) {
+        alert('Contact deleted successfully!');
+      } else {
+        console.error('Failed to delete contact.');
+      }
+    }
+  };
 
   return (
     <tr>
@@ -11,7 +24,7 @@ const Contact = ({ contact, index }) => {
       <td>{email}</td>
       <td >
         <button className='flex mb-1 justify-center items-center gap-6 text-blue-800'>Edit <FaEdit /></button>
-        <button className='flex justify-center items-center gap-2 text-red-700'>Delete <FaTrash /></button>
+        <button onClick={handleDelete} className='flex justify-center items-center gap-2 text-red-700'>Delete <FaTrash /></button>
       </td>
     </tr>
   );
